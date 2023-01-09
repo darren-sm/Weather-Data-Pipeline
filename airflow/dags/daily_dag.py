2be52b14d98c5fea387f2d7181c7a1661e63210d
Darren Malibiran <darrenmalibiran@outlook.com>
2023-01-09 17:27:51 +0800

    Added docstring

--- airflow/dags/daily_dag.py
+++ airflow/dags/daily_dag.py
@@ -11,18 +11,29 @@ import logging
 
 
 def extract_task(execution_date):    
+    """
+    Download the objects modified within the last 24 hours inside an S3 bucket folder of current year.    
+    """
     start = datetime.now()
     year = execution_date.strftime('%Y')
     etl.download_data(year, category = "daily")
     logging.info("Finished extract_task. All downloads saved to airflow/data/raw/%s. Task took up %s seconds", year, (datetime.now() - start).seconds)
 
 def transform_task():
+    """
+    Transform the extracted flat file (from downloaded objects in extract_task) into weather daily summaries.
+    Save the clean version into a local CSV file inside data/clean/current_year.
+    """
     # for year in airflow_dir/data/raw/
     # for file in year folder
     # Use modules.etl.transform(file)
     pass
 
 def upsert_task():
+    """
+    Upsert CSV file into PostgreSQL database.
+    """
+    # https://stackoverflow.com/questions/48019381/how-postgresql-copy-to-stdin-with-csv-do-on-conflic-do-update
     # for file in airflow_dir/data/year/clean
     # Use modules.etl.upsert(file)
     pass
@@ -54,4 +65,5 @@ with local_workflow:
         python_callable = transform_task
     )
 
+    # Download the objects (archives) > Extract the files containing raw weather records > Transform the raw data and save to CSV file > Upsert CSV to PostgreSQL database
     task1 >> task2 >> task3
--- airflow/dags/modules/etl.py
+++ airflow/dags/modules/etl.py
@@ -6,19 +6,49 @@ import numpy as np
 from datetime import datetime, timedelta
 
 def _get_daily_list(folder_objects):
-    for index, obj in enumerate(folder_objects):
-        yesterday = datetime.now() - timedelta(hours=24)
+    """
+    Filter the list of object keys from S3 bucket to download for daily data. If the object's last modified date is more than 24 hours ago, skip it. Else, include it in download list.
+
+    Parameter:
+    ----------
+    folder_objects: generator
+        Generator for object keys (string) inside a folder in S3 bucket
+
+    Yields:
+    ----------
+    S3.Object
+        Objects modified within the last 24 hours
+    """
+    for index, obj in enumerate(folder_objects):        
         # Check if the object's last modified date is more than 24 hours ago
+        yesterday = datetime.now() - timedelta(hours=24)
         if yesterday < obj.last_modified.replace(tzinfo=None):            
             yield obj
     logging.info("Found %s objects modified within the last 24 hours since today %s", index + 1, datetime.now().strftime("%Y-%m-%d %H:%M"))
 
 def download_data(year, category = "historical"):
+    """
+    Download the historical/daily weather data in the form of S3 objects inside the year folder.
+
+    Parameter:
+    ----------
+    year: string
+        targetted year folder inside the S3 bucket
+
+    Returns:
+    ----------
+    None
+
+    Example:
+    ----------
+    >>> # Download historical data for year 2021. Data will be saved in airflow/data/raw/2021
+    >>> download_data("2021")
+    """
     list_of_objects = noaa_isd.list_object_keys(f"isd-lite/data/{year}/")
     if category == "daily":
         logging.info("Category set to daily. Now filtering the list of objects to items modified within the last 24 hours since today %s", datetime.now().strftime("%Y-%m-%d %H:%M"))
         list_of_objects = _get_daily_list(list_of_objects)
-    # object_keys = (obj.key for obj in list_of_objects)
+    
     object_keys = (obj.key for index, obj in enumerate(list_of_objects) if index < 5)
     noaa_isd.download_multiple(object_keys)    
     logging.info("All objects for year %s retrieved from %s and saved to %s local directory", year, f"isd-lite/data/{year}/", f"airflow/data/raw/{year}/")
@@ -27,6 +57,11 @@ def _get_sky_condition(key):
     """
     Fetch the value for an integer sky condition
     
+    Parameter:
+    ----------
+    key: integer
+        Integer representation of sky condition
+
     Returns
     --------- 
     string
@@ -62,19 +97,32 @@ def _get_sky_condition(key):
     return mapping[int(key)]
 
 def transform(filename):
+    """
+    Transform the content of a text-based flat file into day summarization of hourly records. Save the transformed data into a local CSV flat file.
+
+    Parameter:
+    ----------
+    filename: string
+        Name of the flat file containing raw hourly weather data.
+
+    Returns:
+    ----------
+    None
+
+    Example:
+    ----------
+    >>> # Transform the content of "010010-99999-2022" flat file inside "data/daw/2022" folder
+    >>> transform("data/raw/2022/010010-99999-2022")
+    >>> # Transformed data saved in "data/clean/2022/010010-99999-2022.csv"
+    """
     # Create a generator for reading the content of flat file
     file_data = isd_io.read_isd(filename)
 
-    # Create a pandas dataframe from the generator
-    df = pd.DataFrame(file_data)
-
-    # Create a count column to check the no. of records of a given date
-    df['count'] = df.groupby('date')['date'].transform('count')
-
-    # If a day has only less than 4 records, drop them all
-    df = df[df['count'] > 3]
-
-    # Drop 'count' column
+    # Using pandas, create a temp count column which contains the no. of weather records in a given day
+    # If a day has only less than 4 records, drop them all    
+    df = pd.DataFrame(file_data)    
+    df['count'] = df.groupby('date')['date'].transform('count')    
+    df = df[df['count'] > 3]    
     df = df.drop('count', axis = 1)
 
     # Summarize the dataset using the `mean` aggregate function
