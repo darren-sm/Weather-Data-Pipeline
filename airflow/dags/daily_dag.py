3e320261cd50beeb38ea5589fb0695d3b806b3cd
Darren Malibiran <darrenmalibiran@gmail.com>
2023-04-07 11:41:08 +0800

    Removed dependency on now deleted etl module

--- airflow/dags/daily_dag.py
+++ airflow/dags/daily_dag.py
@@ -5,27 +5,44 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 
 # External modules
-from modules import etl
-from datetime import timedelta, datetime
-from modules.decorator import logger
+from modules import noaa_isd
+from modules import isd_io
+import logging
+from datetime import timedelta
+import glob
+import os
 
-@logger
-def extract_task(execution_date):    
+# @logger
+def download_task(execution_date):    
     """
     Download the objects modified within the last 24 hours inside an S3 bucket folder of current year.    
     """
+    # Get the list of objects to download
     year = execution_date.strftime('%Y')
-    # etl.download_data(year, category = "daily")
-    etl.download_data("2022", category = "daily")
+    list_of_objects = noaa_isd.list_object_keys(f"isd-lite/data/{year}/")
+    object_keys = (obj.key for index, obj in enumerate(noaa_isd.get_daily_list(list_of_objects)) if index < 5)
 
-def transform_task():
+    # Folder to save the files. Create it if it does not exist
+    directory = f"{noaa_isd.airflow_dir}/data/raw/{year}"
+    if not os.path.exists(directory):
+        logging.info("Folder for year %s raw data not found. Creating %s now", year, directory)
+        os.makedirs(directory)
+
+    # Download all the objects through multi-threading
+    noaa_isd.download_multiple(object_keys)    
+    logging.info("All objects for year %s retrieved from %s and saved to %s local directory", year, f"isd-lite/data/{year}/", f"airflow/data/raw/{year}/")
+
+
+def transform_task(execution_date):
     """
     Transform the extracted flat file (from downloaded objects in extract_task) into weather daily summaries.
     Save the clean version into a local CSV file inside data/clean/current_year.
     """
-    # for year in airflow_dir/data/raw/
-    # for file in year folder
-    # Use modules.etl.transform(file)
+    directory = f"{noaa_isd.airflow_dir}/data/raw/{execution_date.strftime('%Y')}"
+    for filename in glob.glob(f"{directory}/*"):
+        if not filename.endswith("csv"):
+            isd_io.transform(filename)
+            # delete filename 
     print("Hello from task 3 (Python)")
     pass
 
@@ -52,13 +69,13 @@ local_workflow = DAG(
 with local_workflow:  
     task1 = PythonOperator(
         task_id = "DownloadData",
-        python_callable = extract_task
+        python_callable = download_task
     )
 
     task2 = BashOperator(
         task_id = "ExtractArchive",
         # bash_command = 'echo "Path to raw data files ${AIRFLOW_HOME:-/opt/airflow}/data/raw/{{ execution_date.strftime(\'%Y\') }}"'
-        bash_command = 'echo Found $(eval "find ${AIRFLOW_HOME:-/opt/airflow}/data/raw/2022 -name \'*.gz\' | wc -l") .gz archives in /raw/2022 folder. Extracting them all now. && gunzip -fv ${AIRFLOW_HOME:-/opt/airflow}/data/raw/2022/*.gz'
+        bash_command = 'echo Found $(eval "find ${AIRFLOW_HOME:-/opt/airflow}/data/raw/{{ execution_date.strftime(\"%Y\") }} -name \'*.gz\' | wc -l") .gz archives in /raw/{{ execution_date.strftime(\"%Y\") }} folder. Extracting them all now. && gunzip -fv ${AIRFLOW_HOME:-/opt/airflow}/data/raw/{{ execution_date.strftime(\"%Y\") }}/*.gz'
     )
 
     task3 = PythonOperator(
