997af9cd84f71b2cf608bd26d8cabc3223ecf288
Darren Malibiran <darrenmalibiran@gmail.com>
2023-04-08 15:54:06 +0800

    Insert and Update (upsert) clean weather record to the PSQL database

--- airflow/dags/daily_dag.py
+++ airflow/dags/daily_dag.py
@@ -7,10 +7,14 @@ from airflow.utils.dates import days_ago
 # External modules
 from modules import noaa_isd
 from modules import isd_io
+from modules import postgres
 import logging
 from datetime import timedelta, datetime
 import glob
 import os
+
+CLEAN_CSV_DIRECTORY = f"{noaa_isd.airflow_dir}/data/clean" 
+RAW_FILES_DIRECTORY = f"{noaa_isd.airflow_dir}/data/raw"
 
 # @logger
 def download_task(execution_date):    
@@ -22,15 +26,14 @@ def download_task(execution_date):
     list_of_objects = noaa_isd.list_object_keys(f"isd-lite/data/{year}/")
     object_keys = (obj.key for index, obj in enumerate(noaa_isd.get_daily_list(list_of_objects)) if index < 5)
 
-    # Folder to save the files. Create it if it does not exist
-    directory = f"{noaa_isd.airflow_dir}/data/raw/{year}"
-    if not os.path.exists(directory):
-        logging.info("Folder for year %s raw data not found. Creating %s now", year, directory)
-        os.makedirs(directory)
+    # Folder to save the files. Create it if it does not exist    
+    if not os.path.exists(f"{RAW_FILES_DIRECTORY}/{year}"):
+        logging.info("Folder for year %s raw data not found. Creating %s now", year, f"{RAW_FILES_DIRECTORY}/{year}")
+        os.makedirs(f"{RAW_FILES_DIRECTORY}/{year}")
 
     # Download all the objects through multi-threading
     noaa_isd.download_multiple(object_keys)    
-    logging.info("All objects for year %s retrieved from %s and saved to %s local directory", year, f"isd-lite/data/{year}/", f"airflow/data/raw/{year}/")
+    logging.info("All objects for year %s retrieved from %s and saved to %s local directory", year, f"isd-lite/data/{year}/", f"~/data/raw/{year}/")
 
 
 def transform_task(execution_date):
@@ -41,27 +44,45 @@ def transform_task(execution_date):
 
     # Folder to save the clean files. Create it if it does not exist
     year = execution_date.strftime('%Y')
-    directory = f"{noaa_isd.airflow_dir}/data/clean/{year}"    
-    if not os.path.exists(directory):
-        logging.info("Folder for year %s clean data not found. Creating %s now", year, directory)
-        os.makedirs(directory)
-
-    # List out all the raw files that are not yet cleaned
-    raw_files_directory = f"{noaa_isd.airflow_dir}/data/raw/{execution_date.strftime('%Y')}"
-    for filename in glob.glob(f"{raw_files_directory}/*"):
+       
+    if not os.path.exists(f"{CLEAN_CSV_DIRECTORY}/{year}"):
+        logging.info("Folder for year %s clean data not found. Creating %s now", year, CLEAN_CSV_DIRECTORY)
+        os.makedirs(f"{CLEAN_CSV_DIRECTORY}/{year}")
+   
+    for filename in glob.glob(f"{RAW_FILES_DIRECTORY}/{year}/*"):
         # Transform every non-csv file that are modified within the last 24 hours
         if not filename.endswith("csv") and datetime.now().timestamp() - os.path.getmtime(filename) <= 86400:
             isd_io.transform(filename, year)        
     
 
-def upsert_task():
+def upsert_weather_task(execution_date):
     """
     Upsert CSV file into PostgreSQL database.
     """
-    # https://stackoverflow.com/questions/48019381/how-postgresql-copy-to-stdin-with-csv-do-on-conflic-do-update
-    # for file in airflow_dir/data/year/clean
-    # Use modules.etl.upsert(file)
-    pass
+    # Create a database connection
+    credentials = {
+        "host": "app-db",        
+        "user": os.environ['POSTGRES_USER'],
+        "password": os.environ['POSTGRES_PASSWORD'],
+        "database": os.environ['POSTGRES_DB']
+    }
+    engine = postgres.PsqlEngine(credentials)
+
+    # TSV Files (clean data to upsert)
+    year = execution_date.strftime('%Y')
+    tsv_files = glob.glob(f"{CLEAN_CSV_DIRECTORY}/{year}/*")
+
+    # Upsert the clean csv data to weather table
+    try:
+        for tsv_file in tsv_files:
+            # Select only those that contains the weather data (size not in filename) and modified within the last 24 hours
+            if tsv_file.endswith("tsv") and datetime.now().timestamp() - os.path.getmtime(tsv_file) <= 86400:                
+                with open(tsv_file, 'r') as f:
+                    next(f)
+                    engine.upsert("weather", f)
+    finally:
+        del engine
+
 
 ################# DAG #################
 
@@ -91,5 +112,10 @@ with local_workflow:
         python_callable = transform_task
     )
 
+    task4 = PythonOperator(
+        task_id = "IngestWeatherData",
+        python_callable = upsert_weather_task
+    )
+
     # Download the objects (archives) > Extract the files containing raw weather records > Transform the raw data and save to CSV file > Upsert CSV to PostgreSQL database
-    task1 >> task2 >> task3
+    task1 >> task2 >> task3 >> task4
